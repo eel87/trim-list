d745c11abc06dc5683f3eeaea3f3d2dca0985df4
erinleck@gmail.com <erinleck@gmail.com>
2019-11-19 10:04:09 -0700

    updated docs

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,18 +1,44 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if(user) {
+    // $('.buttons').load("views/user-option-btns.html #buttons");
+  } else {
+    // $('document').load('index.html');
+  }
+});
+
 //signup
-const signupForm = $("#signup-form");
+var signupForm = $("#signup-form")
+signupForm.submit(function(event) { 
+  event.preventDefault();
 
-signupForm.submit(function(e) {
-  e.preventDefault();
-
-  const email = $("#signup-email").val();
-  const password = $("#signup-password").val();
-
-  console.log(name, email, password);
+  var email = $("#signup-email").val();
+  var password = $("#signup-password").val();
 
   // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-    $('.main-content').load('views/create-list-title.html');
-    signupForm.trigger("reset");
-  })
+    // $('.main-content').load('views/create-list-title.html');
+  });
+});
+
+// signin
+var signinForm = $("#signin-form");
+
+signinForm.submit(function(event) {
+  event.preventDefault();
+
+  var email = $("#signin-email").val();
+  var password = $("#signin-password").val();
+  
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    $('.main').load("views/user-option-btns.html");
+  });
+});
+
+// log out
+var logout = $("#signout-btn");
+
+logout.click(function(event) {
+  event.preventDefault();
+  auth.signOut();
 });
--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
   $('.sign-up-btn').click(function() {
     $('.main-content').load("views/sign-up.html");
   }); 
+
+  $('.sign-in-btn').click(function() {
+    $('.main-content').load("views/sign-in.html");
+  })
 });
